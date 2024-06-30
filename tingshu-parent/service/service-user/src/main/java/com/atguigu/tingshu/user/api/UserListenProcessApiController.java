b74bc2e7c40f2790d7968acfda14ade64b081871
PengBin <pengbin2367@163.com>
2024-06-30 17:30:30 +0800

    feat: 引入Mongo保存用户声音播放进度，实现专辑播放

--- tingshu-parent/service/service-user/src/main/java/com/atguigu/tingshu/user/api/UserListenProcessApiController.java
+++ tingshu-parent/service/service-user/src/main/java/com/atguigu/tingshu/user/api/UserListenProcessApiController.java
@@ -1,19 +1,35 @@
 package com.atguigu.tingshu.user.api;
 
+import com.atguigu.tingshu.common.login.GuiguLogin;
+import com.atguigu.tingshu.common.result.Result;
 import com.atguigu.tingshu.user.service.UserListenProcessService;
+import com.atguigu.tingshu.vo.user.UserListenProcessVo;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @Tag(name = "用户声音播放进度管理接口")
 @RestController
-@RequestMapping("api/user/userListenProcess")
+@RequestMapping("/api/user/userListenProcess")
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class UserListenProcessApiController {
 
 	@Autowired
 	private UserListenProcessService userListenProcessService;
 
+	@GuiguLogin
+	@PostMapping("/updateListenProcess")
+	public Result updateListenProcess(@RequestBody UserListenProcessVo userListenProcessVo) {
+		userListenProcessService.updateListenProcess(userListenProcessVo);
+		return Result.ok();
+	}
+
+	@GuiguLogin
+	@GetMapping("/getTrackBreakSecond/{trackId}")
+	public Result<BigDecimal> getTrackBreakSecond(@PathVariable("trackId") Long trackId) {
+		return Result.ok(userListenProcessService.getTrackBreakSecond(trackId));
+	}
 }
 
--- tingshu-parent/service/service-user/src/main/java/com/atguigu/tingshu/user/service/impl/UserListenProcessServiceImpl.java
+++ tingshu-parent/service/service-user/src/main/java/com/atguigu/tingshu/user/service/impl/UserListenProcessServiceImpl.java
@@ -1,9 +1,20 @@
 package com.atguigu.tingshu.user.service.impl;
 
+import com.atguigu.tingshu.common.util.AuthContextHolder;
+import com.atguigu.tingshu.common.util.MongoUtil;
+import com.atguigu.tingshu.model.user.UserListenProcess;
 import com.atguigu.tingshu.user.service.UserListenProcessService;
+import com.atguigu.tingshu.vo.user.UserListenProcessVo;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
 
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
@@ -12,4 +23,39 @@ public class UserListenProcessServiceImpl implements UserListenProcessService {
 	@Autowired
 	private MongoTemplate mongoTemplate;
 
+	@Override
+	public void updateListenProcess(UserListenProcessVo userListenProcessVo) {
+		Long userId = AuthContextHolder.getUserId();
+		// 查询用户播放进度
+		Query query = Query.query(Criteria.where("userId").is(userId).and("trackId").is(userListenProcessVo.getTrackId()));
+		UserListenProcess userListenProcess = mongoTemplate.findOne(query, UserListenProcess.class,
+				MongoUtil.getCollectionName(MongoUtil.MongoCollectionEnum.USER_LISTEN_PROCESS, userId));
+		if (null == userListenProcess) {
+			// 若无则新增
+			userListenProcess = new UserListenProcess();
+			BeanUtils.copyProperties(userListenProcessVo, userListenProcess);
+			userListenProcess.setId(UUID.randomUUID().toString().replaceAll("-", ""));
+			userListenProcess.setUserId(userId);
+			userListenProcess.setIsShow(1);
+			userListenProcess.setCreateTime(new Date());
+			userListenProcess.setUpdateTime(new Date());
+		} else {
+			// 有则更新
+			userListenProcess.setUpdateTime(new Date());
+			userListenProcess.setBreakSecond(userListenProcessVo.getBreakSecond());
+		}
+		mongoTemplate.save(userListenProcess, MongoUtil.getCollectionName(MongoUtil.MongoCollectionEnum.USER_LISTEN_PROCESS, userId));
+	}
+
+	@Override
+	public BigDecimal getTrackBreakSecond(Long trackId) {
+		Long userId = AuthContextHolder.getUserId();
+		Query query = Query.query(Criteria.where("userId").is(userId).and("trackId").is(trackId));
+		UserListenProcess userListenProcess = mongoTemplate.findOne(query, UserListenProcess.class,
+				MongoUtil.getCollectionName(MongoUtil.MongoCollectionEnum.USER_LISTEN_PROCESS, userId));
+		if (null == userListenProcess) {
+			return new BigDecimal(0);
+		}
+		return userListenProcess.getBreakSecond();
+	}
 }
