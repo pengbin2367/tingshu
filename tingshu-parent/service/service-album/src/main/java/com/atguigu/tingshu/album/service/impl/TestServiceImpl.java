0c20ecb89101c999a553783ff603d7d1299e9055
PengBin <pengbin2367@163.com>
2024-07-01 09:44:39 +0800

    test: lua脚本(目前使用单节点Redis解决了多线程并发产生的各种问题（数据丢失、死锁、分布式锁）)

--- tingshu-parent/service/service-album/src/main/java/com/atguigu/tingshu/album/service/impl/TestServiceImpl.java
+++ tingshu-parent/service/service-album/src/main/java/com/atguigu/tingshu/album/service/impl/TestServiceImpl.java
@@ -3,8 +3,11 @@ package com.atguigu.tingshu.album.service.impl;
 import com.atguigu.tingshu.album.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -15,14 +18,22 @@ public class TestServiceImpl implements TestService {
 
     @Override
     public synchronized void setRedis() {
-        Boolean lock = redisTemplate.opsForValue().setIfAbsent("lock", "setnx", 1, TimeUnit.MINUTES);
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        Boolean lock = redisTemplate.opsForValue().setIfAbsent("lock", uuid, 1, TimeUnit.MINUTES);
         if (lock) {
             Integer redisValue = (Integer) redisTemplate.opsForValue().get("tingshu_test");
             if (redisValue != null) {
                 redisValue++;
                 redisTemplate.opsForValue().set("tingshu_test", redisValue);
             }
-            redisTemplate.delete("lock");
+            DefaultRedisScript<Long> script = new DefaultRedisScript<>();
+            script.setScriptText("if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end");
+            script.setResultType(Long.class);
+            redisTemplate.execute(script, List.of("lock"), uuid);
+//            String redisUuid = (String) redisTemplate.opsForValue().get("lock");
+//            if (uuid.equals(redisUuid)) {
+//                redisTemplate.delete("lock");
+//            }
         } else {
             setRedis();
         }
