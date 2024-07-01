a8083137fc91fbb4694cfd6f4a4cf312b53535e1
PengBin <pengbin2367@163.com>
2024-07-01 14:27:55 +0800

    feat: 获取所有vip开通类型

--- tingshu-parent/service/service-user/src/main/java/com/atguigu/tingshu/user/api/VipServiceConfigApiController.java
+++ tingshu-parent/service/service-user/src/main/java/com/atguigu/tingshu/user/api/VipServiceConfigApiController.java
@@ -1,19 +1,32 @@
 package com.atguigu.tingshu.user.api;
 
+import com.atguigu.tingshu.common.cache.GuiguCache;
+import com.atguigu.tingshu.common.login.GuiguLogin;
+import com.atguigu.tingshu.common.result.Result;
+import com.atguigu.tingshu.model.user.VipServiceConfig;
 import com.atguigu.tingshu.user.service.VipServiceConfigService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Tag(name = "VIP服务配置管理接口")
 @RestController
-@RequestMapping("api/user/vipServiceConfig")
+@RequestMapping("/api/user/vipServiceConfig")
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class VipServiceConfigApiController {
 
 	@Autowired
 	private VipServiceConfigService vipServiceConfigService;
 
+	@GuiguCache(prefix = "vipServiceConfig:")
+	@GuiguLogin
+	@GetMapping("/findAll")
+	public Result<List<VipServiceConfig>> findAll() {
+		return Result.ok(vipServiceConfigService.list());
+	}
 }
 
