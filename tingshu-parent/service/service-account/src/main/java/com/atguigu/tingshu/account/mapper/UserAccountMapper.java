e327f4ba690a69e12505f35a15654392c9b092c8
PengBin <pengbin2367@163.com>
2024-07-05 20:33:16 +0800

    feat: 扣减余额

--- tingshu-parent/service/service-account/src/main/java/com/atguigu/tingshu/account/mapper/UserAccountMapper.java
+++ tingshu-parent/service/service-account/src/main/java/com/atguigu/tingshu/account/mapper/UserAccountMapper.java
@@ -3,8 +3,14 @@ package com.atguigu.tingshu.account.mapper;
 import com.atguigu.tingshu.model.account.UserAccount;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
+
+import java.math.BigDecimal;
 
 @Mapper
 public interface UserAccountMapper extends BaseMapper<UserAccount> {
 
+    @Update("update tingshu_account.user_account set available_amount = available_amount - #{money} where user_id = #{userId} and available_amount >= #{money} and is_deleted = 0")
+    int updateAvailableAmount(@Param("userId") Long userId, @Param("money") BigDecimal money);
 }
--- tingshu-parent/service/service-account/src/main/java/com/atguigu/tingshu/account/service/impl/UserAccountServiceImpl.java
+++ tingshu-parent/service/service-account/src/main/java/com/atguigu/tingshu/account/service/impl/UserAccountServiceImpl.java
@@ -1,23 +1,63 @@
 package com.atguigu.tingshu.account.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.tingshu.account.mapper.UserAccountDetailMapper;
 import com.atguigu.tingshu.account.mapper.UserAccountMapper;
 import com.atguigu.tingshu.account.service.UserAccountService;
+import com.atguigu.tingshu.common.execption.GuiguException;
+import com.atguigu.tingshu.common.util.AuthContextHolder;
 import com.atguigu.tingshu.model.account.UserAccount;
+import com.atguigu.tingshu.model.account.UserAccountDetail;
+import com.atguigu.tingshu.model.order.OrderInfo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Slf4j
 @Service
+@Transactional(rollbackFor = Exception.class)
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class UserAccountServiceImpl extends ServiceImpl<UserAccountMapper, UserAccount> implements UserAccountService {
 
 	@Autowired
 	private UserAccountMapper userAccountMapper;
 
+	@Autowired
+	private RedissonClient redissonClient;
+
+	@Autowired
+	private UserAccountDetailMapper userAccountDetailMapper;
+
 	@Override
 	public void decountUserAccount(String msg) {
-
+		OrderInfo orderInfo = JSONObject.parseObject(msg, OrderInfo.class);
+		String orderNo = orderInfo.getOrderNo();
+		RLock lock = redissonClient.getLock("Order_User_Account_Lock_" + orderNo);
+		lock.lock();
+		try {
+			Long userId = AuthContextHolder.getUserId();
+			UserAccountDetail userAccountDetail = userAccountDetailMapper.selectOne(new LambdaQueryWrapper<UserAccountDetail>()
+					.eq(UserAccountDetail::getOrderNo, orderNo)
+					.eq(UserAccountDetail::getUserId, userId));
+			if (userAccountDetail != null) {
+				return ;
+			}
+			BigDecimal orderAmount = orderInfo.getOrderAmount();
+			int i = userAccountMapper.updateAvailableAmount(userId, orderAmount);
+			if (i <= 0) {
+				throw new GuiguException(201, "余额不足");
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			lock.unlock();
+		}
 	}
 }
