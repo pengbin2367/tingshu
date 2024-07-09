838ab22081c19b0eea541cc1c0fb9ed458d95805
PengBin <pengbin2367@163.com>
2024-07-09 13:39:33 +0800

    feat: 获取账户余额

--- tingshu-parent/service/service-account/src/main/java/com/atguigu/tingshu/account/api/UserAccountApiController.java
+++ tingshu-parent/service/service-account/src/main/java/com/atguigu/tingshu/account/api/UserAccountApiController.java
@@ -1,19 +1,32 @@
 package com.atguigu.tingshu.account.api;
 
 import com.atguigu.tingshu.account.service.UserAccountService;
+import com.atguigu.tingshu.common.login.GuiguLogin;
+import com.atguigu.tingshu.common.result.Result;
+import com.atguigu.tingshu.common.util.AuthContextHolder;
+import com.atguigu.tingshu.model.account.UserAccount;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "用户账户管理")
 @RestController
-@RequestMapping("api/account/userAccount")
+@RequestMapping("/api/account/userAccount")
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class UserAccountApiController {
 
 	@Autowired
 	private UserAccountService userAccountService;
 
+	@GuiguLogin
+	@GetMapping("/getAvailableAmount")
+	public Result getAvailableAmount() {
+		UserAccount account = userAccountService.getOne(
+				new LambdaQueryWrapper<UserAccount>().eq(UserAccount::getUserId, AuthContextHolder.getUserId()));
+		return Result.ok(account.getAvailableAmount());
+	}
 }
 
